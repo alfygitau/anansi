2e7bbdab80bcc8d81a973f13749781be2e80911c
japhethkasomo <japhethkasomo@anansitechnology.com>
2026-05-14 22:20:10 +0300

    changes

--- src/pages/products/LoanProducts.jsx
+++ src/pages/products/LoanProducts.jsx
@@ -178,7 +178,7 @@ const LoanProducts = () => {
         </header>
 
         {/* Products Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6">
           {filteredProducts.map((product) => (
             <DetailedProductCard
               key={product.id}
